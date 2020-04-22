210befc650793477bbf18f45a6a97682273fdd33
yammine7 <64126093+yammine7@users.noreply.github.com>
2020-04-22 09:59:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+	<!doctype html>
+	 <head>
+	 	<title>
+	 		when bored open me
+	 	</title>
+	 </head>
+	 <body style="font-size: 100px">
+	 	<h1>the when bored website</h1>
+	 	<p>read me if you are bored.<br> if you are bored watch videos on youtubee.<br>
+	 	you can also search for words in other  languages and speak with your family with the foreign language</p>
+
+	 </body>
